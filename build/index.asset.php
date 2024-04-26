054847a3e9e60a1b1db9021c79ce468eaa1ca30a
Urszula Wielgosz <urszula@wielgosz.info>
2024-04-26 12:21:16 +0000

    Add dependencies

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '582c7871349577200ab0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd3b27025636b8a68c55e');
--- build/render.php
+++ build/render.php
@@ -1,8 +1,18 @@
 <?php
 /**
+ * Query Taxonomy Filter Block Template.
+ *
+ * @param   array $attributes - The block attributes.
+ * @param   string $content - The block default content.
+ * @param   WP_Block $block - The block instance.
+ *
+ * @package \WI\SonxFSE
+ *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Query Taxonomy Filter – hello from a dynamic block!', 'wi-query-tax-filter' ); ?>
-</p>
+
+<ul <?php echo get_block_wrapper_attributes(); ?>>
+	<li><a href="?query-1-page=1" data-wp-key="query-tax-filter-all" data-wp-on--click="core/query::actions.navigate" data-wp-on--mouseenter="core/query::actions.prefetch" data-wp-watch="core/query::callbacks.prefetch">All</a></li>
+	<li><a href="?query-1-page=1&tqt_taxonomy=wi-project-category&gt_term=mentoring"  data-wp-key="query-tax-filter-wi-project-category-mentoring" data-wp-on--click="core/query::actions.navigate" data-wp-on--mouseenter="core/query::actions.prefetch" data-wp-watch="core/query::callbacks.prefetch">Mentoring</a></li>
+</ul>
